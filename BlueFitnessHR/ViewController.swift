bd59363ed3bf8b01afcbabc9e34c0a2f9850a304
Kushangupta <kushangupta@gmail.com>
2018-02-04 13:44:13 -0600

    Initial Commit

---
+++ BlueFitnessHR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlueFitnessHR
+//
+//  Created by Kushan Gupta on 2/4/18.
+//  Copyright © 2018 KBStudios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
