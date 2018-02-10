48d7a726e30fe0c4e396edcae95abdd6ebd4ec34
Kushangupta <kushangupta@gmail.com>
2018-02-10 17:21:43 -0600

    Brett and I got heart Rate woo hoo!

--- BlueFitnessHR WatchKit Extension/InterfaceController.swift
+++ BlueFitnessHR WatchKit Extension/InterfaceController.swift
@@ -23,6 +23,12 @@ class InterfaceController: WKInterfaceController {
     
     let healthKitManager = HealthKitManager.sharedInstance
     
+    var workoutStartDate: Date?
+    
+    var heartRateQuery: HKQuery?
+    
+    var heartRateSamples: [HKQuantitySample] = [HKQuantitySample]()
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
@@ -59,7 +65,6 @@ class InterfaceController: WKInterfaceController {
         }
         isWorkoutInProgress = !isWorkoutInProgress
         self.workoutButton.setTitle(isWorkoutInProgress ? "End Workout" : "Start Workout")
-        print("button tapped")
     }
     
     func createWorkoutSession() {
@@ -70,6 +75,8 @@ class InterfaceController: WKInterfaceController {
         
         do {
         workoutSession = try HKWorkoutSession(configuration: workoutConfiguration)
+            workoutSession?.delegate = self
+            
         } catch {
             print("Exception thrown")
         }
@@ -86,6 +93,7 @@ class InterfaceController: WKInterfaceController {
         }
         
         healthKitManager.healthStore.start(session)
+        self.workoutStartDate = Date()
     }
     
     func endWorkoutSession() {
@@ -94,7 +102,76 @@ class InterfaceController: WKInterfaceController {
             return
         }
         healthKitManager.healthStore.end(session)
+        saveWorkout()
         
     }
+    func saveWorkout() {
+        
+        guard let startDate = workoutStartDate else{
+            return
+        }
+        let workout = HKWorkout(activityType: .other, start: startDate, end: Date())
+        healthKitManager.healthStore.save(workout) { [weak self](success, error) in print("Was save workout successful \(success)")
+            
+            guard let samples = self?.heartRateSamples else {
+                return
+            }
+            
+            self?.healthKitManager.healthStore.add(samples, to: workout, completion: {(success ,error) in if success {
+                print("Successfully saved heart rate samples.")
+                }
+                
+            })
+    }
+    
+}
+}
+
+extension InterfaceController: HKWorkoutSessionDelegate {
+    func workoutSession(_ workoutSession: HKWorkoutSession, didFailWithError error: Error) {
+        print("workout failed with error")
+    }
+    
+    func workoutSession(_ workoutSession: HKWorkoutSession, didChangeTo toState: HKWorkoutSessionState, from fromState: HKWorkoutSessionState, date: Date) {
+        switch toState {
+        case .running:
+            print("workout statrted")
+            guard let workoutStartDate = workoutStartDate else {
+                return
+            }
+            if let query = healthKitManager.createHeartRateStreamingQuery(workoutStartDate) {
+                self.heartRateQuery = query
+                self.healthKitManager.heartRateDelegate = self
+                healthKitManager.healthStore.execute(query)
+            }
+        case .ended:
+            print("workout ended")
+            if let query = self.heartRateQuery {
+                healthKitManager.healthStore.stop(query)
+                
+            }
+        default:
+            print("other workout state")
+            
+        }
+    }
+}
+
+extension InterfaceController: HeartRateDelegate {
+    func heartRateUpdated(heartRateSamples: [HKSample]) {
+        guard let heartRateSamples = heartRateSamples as? [HKQuantitySample] else {
+            return
+        }
+        DispatchQueue.main.async {
+            self.heartRateSamples = heartRateSamples
+            guard let sample = heartRateSamples.first else {
+                return
+            }
+            let value = sample.quantity.doubleValue(for: HKUnit(from: "count/min"))
+            let heartRateString = String(format: "%.00f", value)
+            self.heartRateLabel.setText(heartRateString)
+        }
+    }
+    
 }
 
--- Shared/HealthKitManager.swift
+++ Shared/HealthKitManager.swift
@@ -8,6 +8,10 @@
 import Foundation
 import HealthKit
 
+protocol HeartRateDelegate {
+    func heartRateUpdated(heartRateSamples: [HKSample])
+}
+
 class HealthKitManager: NSObject {
     
     static let sharedInstance = HealthKitManager()
@@ -15,6 +19,10 @@ class HealthKitManager: NSObject {
     private override init() {}
     
     let healthStore = HKHealthStore()
+    
+    var anchor: HKQueryAnchor?
+    
+    var heartRateDelegate: HeartRateDelegate?
     
     func authorizeHealthKit(_ completion: @escaping ((_ success: Bool, _ error: Error?) -> Void)) {
         
@@ -28,6 +36,37 @@ class HealthKitManager: NSObject {
             print("Was authorization successful? \(success)")
             completion(success, error)
         }
+    }
+    
+    func createHeartRateStreamingQuery(_ workoutStartDate: Date) -> HKQuery?{
+        
+        guard let heartRateType: HKQuantityType = HKQuantityType.quantityType(forIdentifier: .heartRate) else {
+            return nil
+        }
+        let datePredicate = HKQuery.predicateForSamples(withStart: workoutStartDate, end: nil, options: .strictEndDate)
+        let compoundPredicate = NSCompoundPredicate(andPredicateWithSubpredicates:[datePredicate])
+        
+        let heartRateQuery = HKAnchoredObjectQuery(type: heartRateType, predicate: compoundPredicate, anchor: nil, limit: Int(HKObjectQueryNoLimit)) { (query, sampleObjects, deletedObjects, newAnchor, error) in
+            
+            guard let newAnchor = newAnchor,
+                let sampleObjects = sampleObjects else {
+                    return
+                    
+            }
+            self.anchor = newAnchor
+            self.heartRateDelegate?.heartRateUpdated(heartRateSamples: sampleObjects)
+        }
+        heartRateQuery.updateHandler = {(query, sampleObjects, deletedObjects, newAnchor, error) -> Void in
+            guard let newAnchor = newAnchor,
+                let sampleObjects = sampleObjects else {
+                    return
+                    
+            }
+            self.anchor = newAnchor
+            self.heartRateDelegate?.heartRateUpdated(heartRateSamples: sampleObjects)
+
+        }
+        return heartRateQuery
     }
 }
 
